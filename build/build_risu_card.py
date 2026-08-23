efa4b712eeb21c3b1ba84f69ca7f2e3ce958f90a
JamesCane <234951635+ChatPoongKun@users.noreply.github.com>
2026-08-23 21:54:10 +0900

    build: move card build entrypoint into build directory

---
+++ build/build_risu_card.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Build HellTrain using the native RisuAI CHARX-compatible implementation."""
+
+from risu_card_native import main
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
